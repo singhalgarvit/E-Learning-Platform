9ad366bb15ee666367ddc4eb9170773e3e8648d0
Garvit Singhal <garvitsinghal305@gmail.com>
2024-12-13 17:01:16 +0530

    initial Commit

---
+++ Server/index.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express=require("express")
+const app=express();
+const bodyParser=require("body-parser")
+const cors = require('cors')
+// const connectDB=require('./database')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+// connectDB();
+
+app.get('/',(req,res)=>{
+    res.send("Yupp!! You are connected successfully")
+})
+
+
+const port=process.env.PORT;
+
+
+app.listen(port,()=>{
+    console.log(`port is connected on ${port}`)
+})
