ab292f34c97af86f55e79d64b05bf2d73eaccea4
Garvit Singhal <garvitsinghal305@gmail.com>
2024-12-15 07:01:34 +0530

    Deploy

--- Server/index.js
+++ Server/index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(
   cors({
-    origin:['http://localhost:3000','https://shiksha-setu-frontend.vercel.app/'],
+    origin:['http://localhost:3000','https://shiksha-setu-frontend.vercel.app'],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true, // Allow cookies if needed
   })
