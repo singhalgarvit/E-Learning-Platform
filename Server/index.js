3bf9caf768617c94a87b1ba7e1578e1aab34f969
Garvit Singhal <garvitsinghal305@gmail.com>
2024-12-15 05:44:05 +0530

    added CORS cofiguration

--- Server/index.js
+++ Server/index.js
@@ -1,28 +1,33 @@
 require("dotenv").config();
-const express=require("express")
-const app=express();
-const bodyParser=require("body-parser")
-const cors = require('cors')
-const connectDB=require('./database')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const connectDB = require("./database");
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(
+  cors({
+    origin:['http://localhost:3000'],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, // Allow cookies if needed
+  })
+);
 connectDB();
 
-app.get('/',(req,res)=>{
-    res.send("Yupp!! You are connected successfully")
-})
+app.get("/", (req, res) => {
+  res.send("Yupp!! You are connected successfully");
+});
 
-const auth = require('./Auth');
-app.use('/auth',auth)
+const auth = require("./Auth");
+app.use("/auth", auth);
 
-const api = require('./APIs');
-app.use('/api',api);
+const api = require("./APIs");
+app.use("/api", api);
 
-const port=process.env.PORT;
+const port = process.env.PORT;
 
-
-app.listen(port,()=>{
-    console.log(`port is connected on ${port}`)
-})
+app.listen(port, () => {
+  console.log(`port is connected on ${port}`);
+});
